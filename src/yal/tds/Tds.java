828282d62620173a7cf1a39cd8944d16c28d40f1
gady5u <emanuel.gady1@etu.univ-lorraine.fr>
2020-01-23 16:10:05 +0100

    Table des symboles

--- src/yal/tds/Tds.java
+++ src/yal/tds/Tds.java
@@ -1,23 +1,78 @@
 package yal.tds;
 
+import yal.exceptions.AnalyseSemantiqueException;
 import yal.tds.entree.Entree;
 import yal.tds.symbole.Symbole;
 
 import java.util.HashMap;
 
 public class Tds {
+    private  int cptDepl = 0;
     private static Tds instance = new Tds();
+    private HashMap<Entree, Symbole> table = new HashMap<Entree, Symbole>();
 
+    /**
+     * Constructeur vide
+     */
+    private Tds() {
+    }
+
+    /**
+     * Retourne la classe TDS
+     * @return
+     */
     public static Tds getInstance() {
         return instance;
     }
-    private Tds() {
-        this.table = new HashMap<>();
+
+    /**
+     * Getter du compteur
+     * @return
+     */
+    public int getCptDepl() {
+        return cptDepl;
     }
 
-    private HashMap<Entree, Symbole> table;
+    /**
+     * Setter du compteur
+     * @param cptDepl
+     */
+    public void setCptDepl(int cptDepl) {
+        this.cptDepl = cptDepl;
+    }
+
+    /**
+     * Permet d'ajouter une Entrée et un Symbole dans la Hashmap
+     * @param e
+     * @param s
+     */
     public void ajouter(Entree e, Symbole s){
          //Vérification de la double déclation à faire
-        this.table.put(e, s);
+        if(!table.containsValue(e)){ //Si la table ne contient pas encore l'entrée alors
+            try {
+                this.table.put(e, s); //ajouter l'entrée et le symbole à la table
+            } catch (AnalyseSemantiqueException ase)  {
+                throw ase;
+            }
+        }
+        else {
+            throw new AnalyseSemantiqueException(e.getLigne(), " Double déclaration de la variable "+ e.getNom());
+        }
+    }
+
+    /**
+     * Obtenir le symbole à partir d'une Entrée
+     * @param e
+     * @return
+     */
+    public Symbole identifier (Entree e){
+        return table.get(e);
+    }
+
+    /**
+     * @return Le deplacement necessaire pour definir toutes nos variables.
+     */
+    public int getTailleZoneVariable() {
+        return this.cptDepl;
     }
 }
